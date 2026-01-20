13ebb434813257ca5368d071a715003f7ad852df
robiiii0 <robin.chabert@epitech.eu>
2026-01-20 22:45:50 +0100

    Update npm run build

--- epitrello/src/app/boards/memberAvatar.tsx
+++ epitrello/src/app/boards/memberAvatar.tsx
@@ -2,14 +2,14 @@ import { Members } from "../lib/board-api";
 
 export function MemberAvatar({ member }: { member: Members }) {
     return (
-            <div title={member.user.name || member.user.email} className="h-8 w-8 rounded-full bg-gray-200 border-2 border-white overflow-hidden">
-        {member.user.profileImage ? (
-            <img src={member.user.profileImage} alt={member.user.name} />
-        ) : (
-            <span className="flex items-center justify-center h-full w-full text-xs font-bold text-gray-600">
-                {(member.user.name || member.user.email)[0].toUpperCase()}
-            </span>
-        )}
-    </div>
+        <div title={member.user.name || member.user.email} className="h-8 w-8 rounded-full bg-gray-200 border-2 border-white overflow-hidden">
+            {member.user.profileImage ? (
+                <img src={member.user.profileImage} alt={member.user.name || ""} />
+            ) : (
+                <span className="flex items-center justify-center h-full w-full text-xs font-bold text-gray-600">
+                    {(member.user.name || member.user.email)[0].toUpperCase()}
+                </span>
+            )}
+        </div>
     );
 }
